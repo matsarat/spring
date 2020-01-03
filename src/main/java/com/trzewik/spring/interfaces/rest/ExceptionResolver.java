9938ad8dd276fd4ce36fcc52297f03fa4f2ac1b7
Czeffik <trzewik93@gmail.com>
2020-01-03 16:10:25 +0100

    Add exception handler for rest controller

---
+++ src/main/java/com/trzewik/spring/interfaces/rest/ExceptionResolver.java
@@ -0,0 +1,27 @@
+package com.trzewik.spring.interfaces.rest;
+
+import com.trzewik.spring.domain.game.GameException;
+import com.trzewik.spring.domain.game.GameRepository;
+import com.trzewik.spring.domain.player.PlayerRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ExceptionResolver extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {GameException.class})
+    protected ResponseEntity<Object> handleBadRequest(GameException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {GameRepository.GameNotFoundException.class, PlayerRepository.PlayerNotFoundException.class})
+    protected ResponseEntity<Object> handleNotFound(Exception ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
--- src/main/java/com/trzewik/spring/interfaces/rest/RestConfiguration.java
+++ src/main/java/com/trzewik/spring/interfaces/rest/RestConfiguration.java
@@ -4,6 +4,7 @@ import com.trzewik.spring.domain.service.GameService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Configuration
 @EnableWebMvc
@@ -11,5 +12,10 @@ public class RestConfiguration {
     @Bean
     GameController gameController(GameService service) {
         return new GameController(service);
+    }
+
+    @Bean
+    ResponseEntityExceptionHandler responseEntityExceptionHandler() {
+        return new ExceptionResolver();
     }
 }
