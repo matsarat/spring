2550ce46cd220a3830f12f12be7cb0a3f34dac2f
Czeffik <trzewik93@gmail.com>
2020-01-04 18:18:09 +0100

    Add flyway configuration, first db migration and log4j properties

---
+++ src/main/resources/db/migration/V0001__Setup_database.sql
@@ -0,0 +1,23 @@
+CREATE TABLE game
+(
+    id                VARCHAR(36) PRIMARY KEY,
+    deck              TEXT,
+    status            TEXT,
+    current_player_id VARCHAR(36),
+    croupier_id       VARCHAR(36)
+);
+
+CREATE TABLE player
+(
+    id   VARCHAR(36) PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE player_game
+(
+    game_id   VARCHAR(36) REFERENCES game (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    player_id VARCHAR(36) REFERENCES player (id) ON UPDATE CASCADE,
+    hand TEXT,
+    move TEXT,
+    CONSTRAINT game_player_pkey PRIMARY KEY (game_id, player_id)
+);
