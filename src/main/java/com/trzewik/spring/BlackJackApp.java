cc43599960eb45f4529a6fb9ef92a8bdf0bf3fd9
Czeffik <trzewik93@gmail.com>
2020-05-01 22:54:31 +0200

    Remove component scan from project and start using servlet auto configurations

--- src/main/java/com/trzewik/spring/BlackJackApp.java
+++ src/main/java/com/trzewik/spring/BlackJackApp.java
@@ -4,19 +4,21 @@ import com.trzewik.spring.domain.DomainConfiguration;
 import com.trzewik.spring.infrastructure.db.DbConfiguration;
 import com.trzewik.spring.interfaces.rest.RestConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Import({
     DomainConfiguration.class,
     RestConfiguration.class,
-    DbConfiguration.class
+    DbConfiguration.class,
+    PropertyPlaceholderAutoConfiguration.class
 })
 @PropertySource("classpath:application.properties")
-@SpringBootApplication
+@SpringBootConfiguration
 public class BlackJackApp {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(BlackJackApp.class, args);
     }
 }
--- src/main/java/com/trzewik/spring/interfaces/rest/RestConfiguration.java
+++ src/main/java/com/trzewik/spring/interfaces/rest/RestConfiguration.java
@@ -4,21 +4,22 @@ import com.trzewik.spring.domain.game.GameService;
 import com.trzewik.spring.domain.player.PlayerService;
 import com.trzewik.spring.interfaces.rest.game.GameController;
 import com.trzewik.spring.interfaces.rest.player.PlayerController;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@Import(SwaggerConfiguration.class)
+@Import({
+    SwaggerConfiguration.class,
+    DispatcherServletAutoConfiguration.class,
+    ServletWebServerFactoryAutoConfiguration.class
+})
 @Configuration
 @EnableWebMvc
 public class RestConfiguration implements WebMvcConfigurer {
@@ -30,21 +31,6 @@ public class RestConfiguration implements WebMvcConfigurer {
     @Bean
     PlayerController playerController(PlayerService playerService) {
         return new PlayerController(playerService);
-    }
-
-    @Bean
-    ServletWebServerFactory servletWebServerFactory() {
-        return new JettyServletWebServerFactory();
-    }
-
-    @Bean
-    DispatcherServletPath dispatcherServletPath(DispatcherServlet dispatcherServlet) {
-        return new DispatcherServletRegistrationBean(dispatcherServlet, "/");
-    }
-
-    @Bean
-    DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
     }
 
     @Override
