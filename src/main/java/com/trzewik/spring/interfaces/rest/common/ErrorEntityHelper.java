86f8aef2cc726bdeead692edd6edca42348ef6e5
Czeffik <trzewik93@gmail.com>
2020-05-25 22:18:53 +0200

    Add private constructor to ErrorEntityHelper

--- src/main/java/com/trzewik/spring/interfaces/rest/common/ErrorEntityHelper.java
+++ src/main/java/com/trzewik/spring/interfaces/rest/common/ErrorEntityHelper.java
@@ -1,8 +1,11 @@
 package com.trzewik.spring.interfaces.rest.common;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorEntityHelper {
     public static ResponseEntity<ErrorDto> create(Exception exception, HttpStatus status) {
         return new ResponseEntity<>(new ErrorDto(exception, status), status);
