898f8de3d621547cffb154255833ffab9ce4d5c5
matsarat <mat.saratowicz@gmail.com>
2020-12-09 21:10:45 +0100

    Change logic that determines winners and losers

--- src/main/java/com/trzewik/spring/domain/game/ResultsHelper.java
+++ src/main/java/com/trzewik/spring/domain/game/ResultsHelper.java
@@ -3,19 +3,15 @@ package com.trzewik.spring.domain.game;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-//TODO probably should be moved to result domain
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ResultsHelper {
 
     static List<Result> createResults(final Game game) throws Result.Exception {
         validateGameStatus(game);
-        return generateResults(game.getPlayers());
+        return generateResults(game.getPlayers(), game);
     }
 
     private static void validateGameStatus(final Game game) throws Result.Exception {
@@ -24,34 +20,24 @@ public class ResultsHelper {
         }
     }
 
-    //TODO what if players have exactly same hand value?
-    private static List<Result> generateResults(final List<PlayerInGame> players) {
-        final List<PlayerInGame> sorted = getSortedPlayersByResults(players);
-        return IntStream.range(0, sorted.size())
-            .mapToObj(index -> {
-                PlayerInGame player = sorted.get(index);
-                return new Result(index + 1, player);
-            })
-            .collect(Collectors.toList());
-    }
-
-    private static List<PlayerInGame> getSortedPlayersByResults(final List<PlayerInGame> players) {
-        final List<PlayerInGame> sorted = new ArrayList<>(getSortedWinners(players));
-        sorted.addAll(getSortedLosers(players));
-        return sorted;
-    }
-
-    private static List<PlayerInGame> getSortedWinners(final List<PlayerInGame> players) {
+    private static List<Result> generateResults(final List<PlayerInGame> players, Game game) {
         return players.stream()
-            .filter(p -> !p.isLooser())
-            .sorted(Comparator.comparing(PlayerInGame::handValue).reversed())
+            .map(player -> checkIfWonOrLost(player, game))
             .collect(Collectors.toList());
     }
 
-    private static List<PlayerInGame> getSortedLosers(final List<PlayerInGame> players) {
-        return players.stream()
-            .filter(PlayerInGame::isLooser)
-            .sorted(Comparator.comparing(PlayerInGame::handValue))
-            .collect(Collectors.toList());
+
+    private static Result checkIfWonOrLost(PlayerInGame player, Game game) {
+        if (player.handValue() == 21) {
+            return new Result(Result.GameResult.WIN, player);
+        } else if (player.isLooser()) {
+            return new Result(Result.GameResult.LOSS, player);
+        } else if (player.handValue() > game.getCroupier().handValue()) {
+            return new Result(Result.GameResult.WIN, player);
+        } else if (player.handValue() < game.getCroupier().handValue()) {
+            return new Result(Result.GameResult.LOSS, player);
+        } else {
+            return new Result(Result.GameResult.DRAW, player);
+        }
     }
 }
